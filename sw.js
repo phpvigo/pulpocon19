3204376ee77411693ccbc123f8116c14b8deca9c
Sergio Carracedo <info@sergiocarracedo.es>
2019-04-11 17:15:54 +0200

    Publish build

--- sw.js
+++ sw.js
@@ -22,16 +22,16 @@ workbox.precaching.precacheAndRoute([
     "revision": "c7a6fcdf5c0d57f85c653de1c0a7271f"
   },
   {
-    "url": "/_nuxt/acfed295cc29e79bb8ae.js",
-    "revision": "1d280dbb92725a8a4753a797fc70a148"
-  },
-  {
     "url": "/_nuxt/b32134f9eae2a77ee8d2.js",
     "revision": "8bcff6c19db7d9152d4d145fb6835675"
   },
   {
     "url": "/_nuxt/cc03f6a35570c38e96f7.js",
     "revision": "75caccb959e78a08502bf8be3791137d"
+  },
+  {
+    "url": "/_nuxt/d88f9de8e6af07074aff.js",
+    "revision": "2c06f6b76c37229215a8f4acd2612a16"
   }
 ], {
   "cacheId": "PHPulpo-Con-2019",
