1f0c7a7bf0d48a52b91115d812e973533fe402b0
Sergio Carracedo <info@sergiocarracedo.es>
2019-04-12 16:20:13 +0200

    Publish build

--- sw.js
+++ sw.js
@@ -2,16 +2,16 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/4776604a634e71be3bf4.js",
-    "revision": "039678829a36abb1af3764ec9ad1ad15"
+    "url": "/_nuxt/3bd082f29c365f39078d.js",
+    "revision": "7a6d7b52954325e0a0f8e75be854fae9"
   },
   {
     "url": "/_nuxt/53679966527c435dcb3a.js",
     "revision": "11ae7d9e170741b5bf188f64cd7e980a"
   },
   {
-    "url": "/_nuxt/56b765df9048b9c1d3b3.js",
-    "revision": "aab04e798371fcba6011f6653ab2241b"
+    "url": "/_nuxt/538aec1958da73c27bf3.js",
+    "revision": "e9e29f3c7a49c84e657ce0166a3b6550"
   },
   {
     "url": "/_nuxt/79ff86439bd8c3b5f5d0.js",
