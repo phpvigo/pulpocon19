71da249c9ce1b0bee4a0d219ddeda85db551d3f1
Sergio Carracedo <info@sergiocarracedo.es>
2019-05-29 15:49:51 +0200

    Publish build

--- sw.js
+++ sw.js
@@ -2,32 +2,40 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/018cbe7a1c7ef994f010.js",
-    "revision": "75caccb959e78a08502bf8be3791137d"
+    "url": "/_nuxt/218bf808d913ac3c0aaf.js",
+    "revision": "d7bef0605f8a79a55cd2a7daf30a0e8a"
   },
   {
-    "url": "/_nuxt/38abbf24f268f08aa85d.js",
-    "revision": "93395aca49276b822a6c7d28448ec34e"
+    "url": "/_nuxt/452ff9822564df62830c.js",
+    "revision": "96ae1eb8e9a2c30dceb4ea59a0e08667"
   },
   {
-    "url": "/_nuxt/79ff86439bd8c3b5f5d0.js",
-    "revision": "59df20265f8decd62e1f72e9e7630e76"
+    "url": "/_nuxt/45e680ceadc85bc26f3d.js",
+    "revision": "6a72c9ef85a271845ce0c4eb9423df8a"
   },
   {
     "url": "/_nuxt/7c22f49f2a7d53318c98.css",
     "revision": "c7a6fcdf5c0d57f85c653de1c0a7271f"
   },
   {
-    "url": "/_nuxt/8a0f6932a23f425addaa.js",
-    "revision": "a65bfafa223eae5c2345e71f184f860d"
+    "url": "/_nuxt/9e4e49c31360aa794481.js",
+    "revision": "f7828bc5984114321397c27dc26af5fc"
   },
   {
-    "url": "/_nuxt/9cdd826c6ab683d5d75c.css",
-    "revision": "2c21c08eab66ffd5b006c76a152969e7"
+    "url": "/_nuxt/b9d9ca19e0ba03854c12.js",
+    "revision": "c200b30e60031e9e001e8ea50af642ef"
   },
   {
-    "url": "/_nuxt/f523257b4cb6212fe7a1.js",
-    "revision": "e117fc03032ba38d23f2bc63838022af"
+    "url": "/_nuxt/c85d7e2c54749ed7af11.js",
+    "revision": "229783da8af7b6e3fad26d46292167b2"
+  },
+  {
+    "url": "/_nuxt/d2e2c275d2c41695912b.css",
+    "revision": "53c54046b0e26aa0b99fde5a2a371f71"
+  },
+  {
+    "url": "/_nuxt/ef40e4045b474f2e8ff4.js",
+    "revision": "191d15185fa9a57a098c94673578e60c"
   }
 ], {
   "cacheId": "PHPulpo-Con-2019",
